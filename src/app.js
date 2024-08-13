a8d6d6e7b38c05b58830817728e6bc685d859f6f
Henna Maria <hennamaria2001@gmail.com>
2024-08-13 15:45:05 +0530

    basic folder structure added

---
+++ src/app.js
@@ -0,0 +1,19 @@
+import cookieParser from 'cookie-parser';
+import express from 'express';
+import cors from 'cors';
+import userRoute from './routes/user.routes.js'
+
+const app = new express();
+
+app.use(cors({
+    origin: true,
+    Credential: true
+}));
+app.use(express.json());
+app.use(express.urlencoded({extended: true, limit: "16kb"}));
+app.use(cookieParser());
+
+// routes
+app.use('/api/v1/users', userRoute)
+
+export default app;
