5f0f2d372c70e55880c186bb8aceeb1b0a1251e1
asha <ashababurajsreelakam@gmail.com>
2024-08-16 21:13:58 +0530

    profile and employment modals

---
+++ src/models/employment.model.js
@@ -0,0 +1,28 @@
+import mongoose, { Schema } from "mongoose";
+
+const employementSchema=new Schema({
+    user: {
+      type: Schema.Types.ObjectId,
+      ref: 'users',
+      required: true
+    },
+    companyName:{
+        type:String
+    },
+    designation:{
+        type:String
+    },
+    employement:{
+        type:String,
+        required:true
+    },
+    location:{
+        type:String
+    },
+    level:{
+        type:String
+    }
+})
+
+const employementModel = new mongoose.model('Employements',employementSchema);
+export default employementModel
---
+++ src/models/profile.model.js
@@ -0,0 +1,66 @@
+import mongoose, { Schema } from "mongoose";
+
+const profileSchema = new Schema({
+    user: {
+      type: Schema.Types.ObjectId,
+      ref: 'users',
+      required: true
+    },
+    dob: {
+      type: Date
+    },
+    bio: {
+      type: String
+    },
+    age: {
+      type: Number,
+      required: true
+    },
+    gender:{
+      type:String,
+      required: true
+    },
+    hobbies:{
+      type:String
+    },
+    qualification: {
+      type: String,
+      required: true
+    },
+    interests: {
+      type: [String]
+    },
+    drinking: {
+      type: String
+    },
+    smoking: {
+      type: String
+    },
+    genderPreference:{
+      type:String
+    },
+    profileImage: {
+      type: String
+    },
+    additionalImage1: {
+      type: String
+    },
+    additionalImage2: {
+      type: String
+    },
+    additionalImage3: {
+      type: String
+    },
+    doNotShowFor:[{ type:Schema.Types.ObjectId, ref: 'users' }],
+    createdAt: {
+      type: Date,
+      default: Date.now
+    },
+    updatedAt: {
+      type: Date,
+      default: Date.now
+    }
+  });
+  const ProfileModel = new mongoose.model('Profile', profileSchema);
+
+  export default ProfileModel;
