4aa21ea541dd3d78f1f3fd2927a322f2b9996e20
Henna Maria <hennamaria2001@gmail.com>
2024-08-19 20:34:41 +0530

    path fixed

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,19 +1,28 @@
 import { Router } from "express";
+import { registerUser, verifyOtp, loginUser } from "../controllers/auth/localAuth.controller.js";
 import { verifyUser } from "../middlewares/verifyjwt.middleware.js";
 import { createProfile } from "../controllers/profile/addProfile.controller.js";
-import upload from "../middlewares/multer.middleware.js"
+import upload from "../middlewares/multer.middleware.js";
 import { relationshipGoalsController } from "../controllers/profile/relationshipGoalsController.js";
 import { setInterest } from "../controllers/profile/interest.controller.js";
 
 const router = new Router();
 
+// Routes from emailotp branch
+router.route("/register").post(registerUser);
+router.route("/verify-otp").post(verifyOtp);
+router.route("/login").post(loginUser);
+
+// Routes from main branch
 router.route('/profile-details').post(verifyUser, upload.fields([
     { name: 'profile', maxCount: 1 },
     { name: 'additionalImg', maxCount: 3 },
     { name: 'reel', maxCount: 1 },
-]), createProfile)
+]), createProfile);
 
 router.route('/relationship-goals/:id').patch(relationshipGoalsController);
-router.patch('/set-interest', verifyUser, setInterest);
 
-export default router
+// New route for setting interest
+router.route('/set-interest').patch(verifyUser, setInterest);
+
+export default router;
