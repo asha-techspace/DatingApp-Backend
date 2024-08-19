063f83edaf01b6572a08dbb9eebc32a201ed86a6
Henna <85387380+Hennamaria07@users.noreply.github.com>
2024-08-19 19:55:21 +0530

    Update db.config.js

--- src/config/db.config.js
+++ src/config/db.config.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 
 const databaseConnection = () => {
-    mongoose.connect(process.env.DB_URL)
+    mongoose.connect(process.env.DATABASE_CONNECTION_URI)
     .then(res => console.log(`DATABASE CONNECTED SUCCESSFUL WITH ${res.connection.host}`))
     .catch(err => console.log(`DATABASE CONNECTION ERROR: ${err.message}`))
 }
