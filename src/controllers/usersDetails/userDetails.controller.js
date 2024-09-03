35c5923a0b712199ef3d102e82c4ec34ed0e281e
Adarsh uk <adarshuk79@gmail.com>
2024-09-03 13:01:11 +0530

    friendreq/remove controller

--- src/controllers/usersDetails/userDetails.controller.js
+++ src/controllers/usersDetails/userDetails.controller.js
@@ -47,9 +47,12 @@ export const users = async (req, res) => {
   
 
   export const userProfile = async (req, res) => {
+    const userId = req.body.userId; // Assuming the userId is passed in req.body.userId
     try {
-      const users = await ProfileModel.findOne({_id: req.params.id}).populate('user');
-      res.status(200).json(users);
+        const user = await ProfileModel.findOne({ 
+            user: req.params.id || userId 
+        }).populate('user');
+      res.status(200).json(user);
     } catch (error) {
       console.error("Error fetching users:", error);
       res.status(500).json({ error: "Failed to fetch users" });
@@ -65,4 +68,16 @@ export const users = async (req, res) => {
       res.status(500).json({ error: "Failed to fetch profiles" });
     }
   };
+
+
+  export const getUserdetails=async(req,res)=>{
+    const userid=req.user
+    try {
+    const user=await UserModel.find({_id:userid})
+    res.status(200).json(user);
+    } catch (error) {
+      console.error("Error fetching user:", error);
+      res.status(500).json({ error: "Failed to fetch user" });
+    }
+  }
   
