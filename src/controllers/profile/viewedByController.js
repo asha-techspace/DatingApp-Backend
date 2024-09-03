8ffb3dd2cdbbadcf8f2fa5425b15dadbdf38a4f1
Henna Maria <hennamaria2001@gmail.com>
2024-09-03 19:50:46 +0530

    additional logic added

--- src/controllers/profile/viewedByController.js
+++ src/controllers/profile/viewedByController.js
@@ -2,21 +2,48 @@ import UserModel from "../../models/user.model.js";
 
 export const viewedBy = async (req, res) => {
     try {
-        const viewedBy = await UserModel.findByIdAndUpdate(req.user._id, { $push: { viewedBy: req.params.id } }, { new: true });
-        if(!viewedBy) {
-            return res.status(404).json({ 
+        // Find the user
+        const user = await UserModel.findById(req.user._id);
+
+        if (!user) {
+            return res.status(404).json({
                 success: false,
                 message: "User not found"
-             });
+            });
         }
+
+        // Check if the user has already been viewed
+        const alreadyViewed = user.viewedBy.some(u => u.toString() === req.params.id);
+
+        if (alreadyViewed) {
+            return res.status(400).json({
+                success: false,
+                message: "User has already been viewed"
+            });
+        }
+
+        // Update the viewedBy list
+        const updatedUser = await UserModel.findByIdAndUpdate(
+            req.user._id,
+            { $push: { viewedBy: req.params.id } },
+            { new: true }
+        );
+
+        if (!updatedUser) {
+            return res.status(404).json({
+                success: false,
+                message: "User not found"
+            });
+        }
+
         return res.status(200).json({
             success: true,
-            data: viewedBy
+            data: updatedUser
         });
     } catch (error) {
-        res.status(500).json({
+        return res.status(500).json({
             success: false,
             message: error.message
-         });
+        });
     }
-}
+};
