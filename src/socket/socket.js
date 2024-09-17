ee199ff4225427013b6fc8ee15724a10ef2cabce
Adarsh uk <adarshuk79@gmail.com>
2024-09-17 10:02:06 +0530

    sort/filter changes

--- src/socket/socket.js
+++ src/socket/socket.js
@@ -1,42 +1,55 @@
 import express from "express";
 import http from "http";
 import { Server } from "socket.io";
+import cors from "cors";
 
 const app = new express();
 const server = http.createServer(app);
-const io = new Server(server, {
 
+// CORS middleware for Express
+app.use(
+  cors({
+    origin: "http://localhost:5173",  // Allow frontend origin
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
+    credentials: true                 // Allow cookies and credentials
+  })
+);
+
+// Socket.IO server with CORS
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:5173",  // Allow frontend origin
+    methods: ["GET", "POST"],
+    credentials: true
+  }
 });
 
 let users = [];
 
-
 const addUser = (userId, socketId) => {
-    !users.some((user) => user.userId === userId) &&
-        users.push({ userId, socketId });
+  !users.some((user) => user.userId === userId) &&
+    users.push({ userId, socketId });
 };
 
 const removeUser = (socketId) => {
-    users = users.filter(user => user.socketId !== socketId);
+  users = users.filter(user => user.socketId !== socketId);
 };
 
 const getUser = (userId) => {
-    return users.find(user => user.userId === userId);
+  return users.find(user => user.userId === userId);
 };
 
-
-
-
-// const userSocketMap = {}  // {userId: SocketId}
+// Socket.IO connection
 io.on("connection", (socket) => {
   console.log(`User Connected: ${socket.id}`);
 
-  // socket.on("join_room", (data) => {
-  //   socket.join(data);
-  // });
-
   socket.on("sendMessage", (data) => {
-    socket.broadcast.emit("getMessage",data)
+    socket.broadcast.emit("getMessage", data);
+  });
+
+  socket.on("disconnect", () => {
+    console.log(`User Disconnected: ${socket.id}`);
+    removeUser(socket.id);
   });
 });
 
